6629f87425a21ff14d3cde15140a55fd3f9f4181
unknown <saravana45454@gmail.com>
2024-09-03 23:51:11 +0530

    second commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template, redirect, url_for
+import random
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST','GET'])
+def home():
+    if request.method == ['POST','GET']:
+        name1 = request.form.get('name1')
+        name2 = request.form.get('name2')
+        
+        return redirect(url_for('form', name1=name1, name2=name2))
+    return render_template('index.html')
+@app.route('/formpage')
+def form():
+    name1 = request.args.get('name1')
+    name2 = request.args.get('name2')
+    score = random.randint(1,100)
+
+    values = {
+        'friend_one' : name1,
+        'friend_two' : name2,
+        'scores'     : score
+    }
+    return render_template('metor.html', item = name1, item2 = name2, score = score)
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
